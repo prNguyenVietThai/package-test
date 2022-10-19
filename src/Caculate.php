e39741c8dea0067300339568396fa0aa392d337f
prNguyenVietThai <thai.nguyenviet@prtimes.co.jp>
2022-10-19 12:41:36 +0900

    add test-function

---
+++ src/Caculate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Thaivietnguyen\PackageTest;
+
+class PRTimesClient
+{
+    /**
+     * Sum
+     *
+     * @param $num1
+     * @param $num2
+     *
+     * @return mixed
+     */
+    public function sum($num1, $num2)
+    {
+        return $num1 + $num2;
+    }
+}
